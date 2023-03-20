623a6aac94b0bbf7146056d046e6b407447fd127
Yande567 <ymusonda2001@gmail.com>
2023-03-20 15:51:33 +0200

    Updated Settings.py

--- superlists/settings.py
+++ superlists/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-*bx&)i*)2vkv77f($=fo$0x47y9*i#ph(kg2#y1y^pqg%-a6=@"
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
